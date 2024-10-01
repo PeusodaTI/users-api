89cb37e7505b3e7ae524b10cbb51cb9865d32e0f
DESKTOP-1VV4LDO\Pedro <peusodati@gmail.com>
2024-09-30 21:44:54 -0300

    feat: iniciando o servidor com express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World")
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const app = express()
+
+const port = process.env.PORT || 3333
+
+app.get('/', (request, response) => {
+    response.send('Ok')
+})
+
+app.listen(port, () => {
+    console.log('App em execução.')
+})
