97e74c80d699703320c52b3b9aec291ceaf7d995
DESKTOP-1VV4LDO\Pedro <peusodati@gmail.com>
2024-10-08 13:43:18 -0300

    feat: garantindo que o PostgressGetUserReposity busque usuários no postgress usando o PrismaClient

--- src/repositories/get-users/postgres-get-users.ts
+++ src/repositories/get-users/postgres-get-users.ts
@@ -1,16 +1,14 @@
+import { prisma } from '../../config/database'
 import { IGetUserRepository } from '../../controllers/get-users/protocols'
 import { User } from '../../models/user'
 
 export class PostgresGetUsersRepository implements IGetUserRepository {
     async getUser(): Promise<User[]> {
-        return [
-            {
-                firstName: 'Pedro',
-                lastName: 'Henrique',
-                email: 'pedro.henrique@email.com',
-                password: 'testPostgres',
-            }
-        ]
+        const postgresDB = prisma
+
+        const users = await postgresDB.user.findMany()
+        
+        return users
     }
 
 }
